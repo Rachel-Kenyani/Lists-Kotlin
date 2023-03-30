b0783ba643f873f03afb1492f835aa9bf6074619
Rachel Kenyani <rachelkenyani@gmail.com>
2023-03-30 18:03:08 +0300

    changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,10 @@ fun main() {
     colors(listOf("red","blue","brown","pink","black","white","yellow","grey","green","purple"))
 
     tall(listOf(167,150,170,200))
+
+    arrange()
+
+    vehicles()
 }
 //Given a list of 10 strings, write a function that returns a list of the strings
 //at even indices ie index 2,4,6 etc
@@ -31,11 +35,11 @@ var total=name.sum()
 
 //Given a list of Person objects, each with the attributes, name, age,
 //height and weight. Sort the list in order of descending age
-data class Person(var name:String, var age:Int,var height:Int,var weight:Double)
-    fun arrange(num:List<Int>): List<Person>{
+open class Person(var name:String, var age:Int,var height:Int,var weight:Double)
+    fun arrange(): List<Person>{
     var order1=Person("Abel",21,170,70.3)
     var order2=Person("Anna",90,150,65.0)
-        var order3=Person("Alice",15,160,40.5)
+    var order3=Person("Alice",15,160,40.5)
 
         var order= listOf<Person>(order1,order2,order3)
         var person=order.sortedByDescending{p -> p.age}
@@ -43,28 +47,34 @@ data class Person(var name:String, var age:Int,var height:Int,var weight:Double)
     return person
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//4. Given a list similar to the one above, write a function in which you will
+//Given a list similar to the one above, write a function in which you will
 //create 2 more people objects and add them to the list at one go.
-//(1 points)
-//
-//5. Write a function that takes in a list of Car objects each with a
+class newPerson(name:String, age:Int,height:Int,weight:Double,gender:String,tribe:String):Person(name,age,height,weight)
+
+fun attributes(attribute:listOf){
+
+}
+
+// Write a function that takes in a list of Car objects each with a
 //registration and mileage attribute and returns the average mileage of
 //all the vehicles in the list.
+data class Car(var registration:String,var mileage:Int)
+fun vehicles():Double{
+    var machine1=Car("KDD 300",2)
+    var machine2=Car("KDA 4550",6)
+    var machine3=Car("KAA 3100",3)
+    var machine4=Car("KBA 4550",1)
 
+    var machine= listOf<Car>(machine1,machine2,machine3,machine4)
+
+    var add=0.0
+    for (m in machine){
+        add+=m.mileage
+    }
+    var avg=add/machine.size
+
+    return avg
+}
 
 
 
